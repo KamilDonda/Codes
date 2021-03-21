4803873f31fd698b2a3b355d28d128da28f66a58
Kwolik <kwolikzsp6@gmail.com>
2021-03-21 18:07:51 +0100

    add my test

--- App.js
+++ App.js
@@ -2,20 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
   return (
     <View style={styles.container}>
       <Text>Test</Text>
+      <Text>Działa u mnie - Robert Kwoll</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
