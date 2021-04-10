b7647d32ccf7afda9238d74b302ee301c8b789c4
Kwolik <kwolikzsp6@gmail.com>
2021-04-10 21:49:18 +0200

    Edit return

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Menu from '../Codes/Menu';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Menu from "../Codes/Menu";
 
 export default function App() {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
     },
   });
 
   return (
     <View style={styles.container}>
-    <Menu />
-      <Text>Test</Text>
-      <Text>Działa u mnie - Robert Kwoll</Text>
-      <StatusBar style="auto" />
+      <Menu />
+      <View style={styles.container}>
+        <Text>Test</Text>
+        <Text>Działa u mnie - Robert Kwoll</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
