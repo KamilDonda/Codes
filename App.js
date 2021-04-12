3a11006e5f5f517f613e4f21d660db957a078951
Kwolik <kwolikzsp6@gmail.com>
2021-04-12 17:45:55 +0200

    Moving the route to the menu

--- App.js
+++ App.js
@@ -1,24 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Menu from "../Codes/Menu";
+import Menu from "./Menu";
 
 export default function App() {
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
-
-  return (
-    <View style={styles.container}>
-      <Menu />
-      <View style={styles.container}>
-        <Text>Test</Text>
-        <StatusBar style="auto" />
-      </View>
-    </View>
-  );
+  return <Menu/>;
 }
