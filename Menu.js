464b0cb28d5d8215414e0f1f2e59f1aa4fba6b73
Kwolik <kwolikzsp6@gmail.com>
2021-04-13 11:38:20 +0200

    Create stacks gallery, history and authors

--- Menu.js
+++ Menu.js
@@ -7,6 +7,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import Scanning from "./screens/Scanning";
 import Generating from "./screens/Generating";
+import Gallery from "./screens/Gallery";
+import History from "./screens/History";
+import Authors  from "./screens/Authors";
 import { DrawerContent } from "./DrawerContent";
 
 const Stack = createStackNavigator();
@@ -27,6 +30,9 @@ export default function MainTabScreen() {
       <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
         <Drawer.Screen name="Scan" component={StackScanning} />
         <Drawer.Screen name="Generate" component={StackGenerating} />
+        <Drawer.Screen name="Gallery" component={StackGallery} />
+        <Drawer.Screen name="History" component={StackHistory} />
+        <Drawer.Screen name="Authors" component={StackAuthors} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
@@ -80,6 +86,102 @@ const StackGenerating = ({ navigation }) => (
     <Stack.Screen
       name="Generowanie kodu"
       component={Generating}
+      options={{
+        headerLeft: () => (
+          <MaterialIcons
+            name="menu"
+            size={40}
+            onPress={() => navigation.openDrawer()}
+            style={styles.menu}
+          />
+        ),
+        headerRight: () => (
+          <MaterialIcons name="lightbulb" size={40} style={styles.bulb} />
+        ),
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const StackGallery = ({ navigation }) => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "#cc0033",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+    }}
+  >
+    <Stack.Screen
+      name="Galeria"
+      component={Gallery}
+      options={{
+        headerLeft: () => (
+          <MaterialIcons
+            name="menu"
+            size={40}
+            onPress={() => navigation.openDrawer()}
+            style={styles.menu}
+          />
+        ),
+        headerRight: () => (
+          <MaterialIcons name="lightbulb" size={40} style={styles.bulb} />
+        ),
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const StackHistory = ({ navigation }) => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "#009900",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+    }}
+  >
+    <Stack.Screen
+      name="Historia"
+      component={History}
+      options={{
+        headerLeft: () => (
+          <MaterialIcons
+            name="menu"
+            size={40}
+            onPress={() => navigation.openDrawer()}
+            style={styles.menu}
+          />
+        ),
+        headerRight: () => (
+          <MaterialIcons name="lightbulb" size={40} style={styles.bulb} />
+        ),
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const StackAuthors  = ({ navigation }) => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "#9900ff",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+    }}
+  >
+    <Stack.Screen
+      name="Autorzy"
+      component={Authors}
       options={{
         headerLeft: () => (
           <MaterialIcons
