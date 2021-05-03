2be20350edf054387e9d8491a51a9658ce56caea
Kamil Donda <dondakamil@gmail.com>
2021-05-03 12:24:18 +0200

    Add scan border

--- screens/Scanning.js
+++ screens/Scanning.js
@@ -2,11 +2,35 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
+const opacity = 'rgba(0, 0, 0, .6)'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
+  },
+  layerTop: {
+    flex: 0.5,
+    backgroundColor: opacity,
+  },
+  layerCenter: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  layerLeft: {
+    flex: 1,
+    backgroundColor: opacity,
+  },
+  focused: {
+    flex: 10,
+  },
+  layerRight: {
+    flex: 1,
+    backgroundColor: opacity,
+  },
+  layerBottom: {
+    flex: 0.5,
+    backgroundColor: opacity,
   },
 });
 
@@ -36,10 +60,19 @@ export default function Scanner({ navigation }) {
 
   return (
     <View style={styles.container}>
+      <View style={styles.rectangle}></View>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFill}
-      />
+      >
+        <View style={styles.layerTop} />
+        <View style={styles.layerCenter}>
+          <View style={styles.layerLeft} />
+          <View style={styles.focused} />
+          <View style={styles.layerRight} />
+        </View>
+        <View style={styles.layerBottom} />
+      </BarCodeScanner>
       {scanned && (
         <Button
           title={"Naciśnij, aby zeskanować ponownie"}
