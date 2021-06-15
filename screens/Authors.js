0c5f482b7e399e7c322e7ceed90a5af6bb4849ea
YELOYELO99 <61738650+YELOYELO99@users.noreply.github.com>
2021-06-15 18:52:58 +0200

    Added few fixes
    Same background set as on other screens and image moved to 'assets'.

--- screens/Authors.js
+++ screens/Authors.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { StyleSheet, Image, View, Text } from "react-native";
-
 export default function Authors () {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: 'flex-start',
-      backgroundColor: 'white',
     },
     authorsStyle: {
       marginLeft: 16,
@@ -24,6 +22,7 @@ export default function Authors () {
     imageStyle: {
       width: 400, 
       height: 320,
+      resizeMode: 'contain',
     },
   },);
 
@@ -32,7 +31,7 @@ export default function Authors () {
     
     <View style={styles.container}>
       <Text style={styles.authorsStyle}> Organizacje: </Text>
-      <Image style={styles.imageStyle} source={require('./images/authorsThings.png')}/>
+      <Image style={styles.imageStyle} source={require('../assets/authorsAsset.png')}/>
       <Text style={styles.authorsStyle}> Autorzy: </Text>
       <Text style={styles.bigPurple}>• Kamil Donda  </Text>
       <Text style={styles.bigPurple}>• Robert Kwoll  </Text>
